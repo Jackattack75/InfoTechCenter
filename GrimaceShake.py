be93c3380e76a422f4878363e1c86bb5ea953e5d
Sutton <jackevansutton@gmail.com>
2024-02-20 13:23:18 -0500

    Created gasLevelAlert() = Declared two vrbls for low and qtr tank - stbl

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -5,10 +5,10 @@ print("Gasoline Branch\n\n")
 import random
 
 #Function that lists Gas Levels, randomly choosing one and returning its value
-def GasLevelGauge():
-    GasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
-    CurrentGasLevel = random.choice(GasLevelList)
-    return CurrentGasLevel
+def gasLevelGauge():
+    gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
+    currentGasLevel = random.choice(gasLevelList)
+    return currentGasLevel
 
 #Function that lists Gas Stations, randomly choosing one and returning its value
 def listOfGasStations():
@@ -16,6 +16,13 @@ def listOfGasStations():
     gasStationNearby = random.choice(gasStations)
     return gasStationNearby
 
+#Function will call the gasLevelGauge to determin our gas level and then find a close gas station
+#by calling the listOfGasStations function if we are on Low or Quarter Tank
+def gasLevelAlert():
+    milesToGasStationsLow = random.uniform( 1, 25)
+    milesToGasStationsQuarterTank = random.uniform(25.1, 50)
+    #gasLevelGauge = gasLevelGauge()
+    print(milesToGasStationsLow)
+    print(milesToGasStationsQuarterTank)
 
-print(GasLevelGauge())
-print(listOfGasStations())
+gasLevelAlert()
